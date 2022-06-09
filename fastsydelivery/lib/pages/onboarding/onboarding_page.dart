2d05c41082de7f699556eb573155bfee5a10b570
Adriano D'lucca Binhara <62717202+AdrianoBinhara@users.noreply.github.com>
2022-06-09 00:55:28 -0300

    configurando projeto

---
+++ fastsydelivery/lib/pages/onboarding/onboarding_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/cupertino.dart';
+
+class OnboardingPage extends StatefulWidget {
+  const OnboardingPage({Key? key}) : super(key: key);
+
+  @override
+  State<OnboardingPage> createState() => _OnboardingPageState();
+}
+
+class _OnboardingPageState extends State<OnboardingPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
